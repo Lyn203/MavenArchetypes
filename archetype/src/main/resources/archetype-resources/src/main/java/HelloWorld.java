a4cf8d46b14ada133a83cde298514280a80f3670
Ahri <Lyn203@users.noreply.github.com>
2019-03-23 22:15:23 +0700

    Add files via upload

---
+++ archetype/src/main/resources/archetype-resources/src/main/java/HelloWorld.java
@@ -0,0 +1,18 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HelloWorld {
+	@RequestMapping(value="/", method = RequestMethod.GET)
+	public String printHello(ModelMap model) {
+		model.addAttribute("msg", "Hello World");
+		return "index";
+	}
+}
---
+++ archetype/src/main/resources/archetype-resources/src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,6 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<h1>${symbol_dollar}{msg}</h1>
